23863db8aef4552d6e115f33eb23d17ba407fc6c
NolanGougeon <gnolan@hotmail.fr>
2018-03-08 19:22:48 +0100

    1st commit

---
+++ Traitement.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace alimenterBaseDeDonnees
+{
+    partial class Traitement
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRepertoireFichiers = new System.Windows.Forms.Label();
+            this.lblFichierCsv = new System.Windows.Forms.Label();
+            this.lblClasse = new System.Windows.Forms.Label();
+            this.cbSuppression = new System.Windows.Forms.CheckBox();
+            this.clbFichierCsv = new System.Windows.Forms.CheckedListBox();
+            this.cbClasse = new System.Windows.Forms.ComboBox();
+            this.btIntegration = new System.Windows.Forms.Button();
+            this.tbRepertoireFichiers = new System.Windows.Forms.TextBox();
+            this.btParcourir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRepertoireFichiers
+            // 
+            this.lblRepertoireFichiers.AutoSize = true;
+            this.lblRepertoireFichiers.Location = new System.Drawing.Point(12, 25);
+            this.lblRepertoireFichiers.Name = "lblRepertoireFichiers";
+            this.lblRepertoireFichiers.Size = new System.Drawing.Size(121, 13);
+            this.lblRepertoireFichiers.TabIndex = 0;
+            this.lblRepertoireFichiers.Text = "Répertoire des fichiers : ";
+            // 
+            // lblFichierCsv
+            // 
+            this.lblFichierCsv.AutoSize = true;
+            this.lblFichierCsv.Location = new System.Drawing.Point(12, 86);
+            this.lblFichierCsv.Name = "lblFichierCsv";
+            this.lblFichierCsv.Size = new System.Drawing.Size(115, 13);
+            this.lblFichierCsv.TabIndex = 1;
+            this.lblFichierCsv.Text = "Fichier CSV à intégrer :";
+            // 
+            // lblClasse
+            // 
+            this.lblClasse.AutoSize = true;
+            this.lblClasse.Location = new System.Drawing.Point(12, 182);
+            this.lblClasse.Name = "lblClasse";
+            this.lblClasse.Size = new System.Drawing.Size(98, 13);
+            this.lblClasse.TabIndex = 2;
+            this.lblClasse.Text = "Choisir une classe :";
+            // 
+            // cbSuppression
+            // 
+            this.cbSuppression.AutoSize = true;
+            this.cbSuppression.Location = new System.Drawing.Point(15, 245);
+            this.cbSuppression.Name = "cbSuppression";
+            this.cbSuppression.Size = new System.Drawing.Size(313, 17);
+            this.cbSuppression.TabIndex = 3;
+            this.cbSuppression.Text = "Suppression de tous les élèves de la classe avant intégration";
+            this.cbSuppression.UseVisualStyleBackColor = true;
+            // 
+            // clbFichierCsv
+            // 
+            this.clbFichierCsv.FormattingEnabled = true;
+            this.clbFichierCsv.Location = new System.Drawing.Point(134, 86);
+            this.clbFichierCsv.Name = "clbFichierCsv";
+            this.clbFichierCsv.Size = new System.Drawing.Size(277, 64);
+            this.clbFichierCsv.TabIndex = 5;
+            // 
+            // cbClasse
+            // 
+            this.cbClasse.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbClasse.FormattingEnabled = true;
+            this.cbClasse.Location = new System.Drawing.Point(134, 179);
+            this.cbClasse.Name = "cbClasse";
+            this.cbClasse.Size = new System.Drawing.Size(91, 21);
+            this.cbClasse.TabIndex = 6;
+            // 
+            // btIntegration
+            // 
+            this.btIntegration.Location = new System.Drawing.Point(140, 289);
+            this.btIntegration.Name = "btIntegration";
+            this.btIntegration.Size = new System.Drawing.Size(121, 23);
+            this.btIntegration.TabIndex = 7;
+            this.btIntegration.Text = "Lancer l\'intégration";
+            this.btIntegration.UseVisualStyleBackColor = true;
+            this.btIntegration.Click += new System.EventHandler(this.btIntegration_Click);
+            // 
+            // tbRepertoireFichiers
+            // 
+            this.tbRepertoireFichiers.Location = new System.Drawing.Point(134, 22);
+            this.tbRepertoireFichiers.Name = "tbRepertoireFichiers";
+            this.tbRepertoireFichiers.ReadOnly = true;
+            this.tbRepertoireFichiers.Size = new System.Drawing.Size(208, 20);
+            this.tbRepertoireFichiers.TabIndex = 8;
+            // 
+            // btParcourir
+            // 
+            this.btParcourir.Location = new System.Drawing.Point(348, 22);
+            this.btParcourir.Name = "btParcourir";
+            this.btParcourir.Size = new System.Drawing.Size(75, 23);
+            this.btParcourir.TabIndex = 9;
+            this.btParcourir.Text = "Parcourir";
+            this.btParcourir.UseVisualStyleBackColor = true;
+            this.btParcourir.Click += new System.EventHandler(this.btParcourir_Click);
+            // 
+            // Traitement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 324);
+            this.Controls.Add(this.btParcourir);
+            this.Controls.Add(this.tbRepertoireFichiers);
+            this.Controls.Add(this.btIntegration);
+            this.Controls.Add(this.cbClasse);
+            this.Controls.Add(this.clbFichierCsv);
+            this.Controls.Add(this.cbSuppression);
+            this.Controls.Add(this.lblClasse);
+            this.Controls.Add(this.lblFichierCsv);
+            this.Controls.Add(this.lblRepertoireFichiers);
+            this.Name = "Traitement";
+            this.Text = "Alimentation de la base de données : Nouvelle classe";
+            this.Load += new System.EventHandler(this.Traitement_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRepertoireFichiers;
+        private System.Windows.Forms.Label lblFichierCsv;
+        private System.Windows.Forms.Label lblClasse;
+        private System.Windows.Forms.CheckBox cbSuppression;
+        private System.Windows.Forms.CheckedListBox clbFichierCsv;
+        private System.Windows.Forms.ComboBox cbClasse;
+        private System.Windows.Forms.Button btIntegration;
+        private System.Windows.Forms.TextBox tbRepertoireFichiers;
+        private System.Windows.Forms.Button btParcourir;
+    }
+}
+
---
+++ Traitement.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using MySql.Data.MySqlClient;
+using System.Security.Cryptography;
+
+namespace alimenterBaseDeDonnees
+{
+    public partial class Traitement : Form
+    {
+        public Traitement()
+        {
+            InitializeComponent();
+        }
+
+        private void btParcourir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog parcourir = new FolderBrowserDialog();
+            parcourir.ShowDialog();
+            tbRepertoireFichiers.Text = parcourir.SelectedPath;
+            string[] filepaths = Directory.GetFiles(parcourir.SelectedPath, "*.csv", SearchOption.TopDirectoryOnly);
+            for (int i = 0; i < filepaths.Length; i++)
+            {
+                clbFichierCsv.Items.Add(filepaths[i].Replace(parcourir.SelectedPath + "\\", ""));
+            }
+        }
+
+        private void Traitement_Load(object sender, EventArgs e)
+        {
+            string sConnexion;
+            MySqlConnection connexion;
+            sConnexion = "user=admin;password=siojjr;database=rallyelecture;host=172.16.0.159";
+            connexion = new MySqlConnection(sConnexion);
+            connexion.Open();
+            string sSelectClasse = "select classe.id,niveauScolaire,anneeScolaire from niveau inner join classe on niveau.id=classe.idNiveau;";
+            MySqlCommand SelectClasse = new MySqlCommand(sSelectClasse, connexion);
+            MySqlDataReader reader = SelectClasse.ExecuteReader();
+            while (reader.Read())
+            {
+                cbClasse.Items.Add(reader["id"]+" "+reader["niveauScolaire"]+" "+reader["anneeScolaire"]);
+            }
+            reader.Close();
+            connexion.Close();
+            renameAuteur();
+        }
+
+        private void btIntegration_Click(object sender, EventArgs e)
+        {
+            string classeNiveau = Convert.ToString(cbClasse.SelectedItem);
+            string[] test = classeNiveau.Split(' ');
+            bool remiseABlanc = false;
+            string sConnexion;
+            MySqlConnection connexion;
+            sConnexion = "user=admin;password=siojjr;database=rallyelecture;host=172.16.0.159";
+            connexion = new MySqlConnection(sConnexion);
+            connexion.Open();
+            if (cbSuppression.Checked == true)
+            {
+                try
+                {
+                    
+                    MySqlCommand DeleteAauthUsersInGroups = new MySqlCommand("delete from aauth_user_to_group where user_id in(select idAuth from eleve where idClasse=@classe)",connexion);
+                    DeleteAauthUsersInGroups.Parameters.AddWithValue("@classe", test[0]);
+                    DeleteAauthUsersInGroups.ExecuteNonQuery();
+                    MySqlCommand DeleteAauthUsers = new MySqlCommand("delete from aauth_users where id in (select idauth from eleve where idclasse=@classe)",connexion);
+                    DeleteAauthUsers.Parameters.AddWithValue("@classe",test[0]);
+                    DeleteAauthUsers.ExecuteNonQuery();
+                    string sSupprimerEleve = "delete from eleve where idClasse in(select classe.id from Classe inner join niveau on niveau.id=classe.idNiveau where anneeScolaire=@annee and niveauScolaire=@niveau);";
+                    MySqlCommand supprimerEleve = new MySqlCommand(sSupprimerEleve, connexion);
+                    supprimerEleve.Parameters.AddWithValue("@annee", test[2]);
+                    supprimerEleve.Parameters.AddWithValue("@niveau",test[1]);
+                    supprimerEleve.ExecuteNonQuery();
+                    
+                    remiseABlanc = true;
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+                
+            }
+            
+
+            String[] lines = System.IO.File.ReadAllLines(tbRepertoireFichiers.Text+"\\"+clbFichierCsv.SelectedItem.ToString());
+            foreach (string  line in lines)
+            {
+                string[] csvlecture = line.Split(';');
+                string nom = csvlecture[1];
+                string prenom = csvlecture[2];
+                string mail = csvlecture[3];
+                MySqlCommand InsertUsers = new MySqlCommand("Insert into aauth_users(email,pass) values (@email,@pass)", connexion);
+                InsertUsers.Parameters.AddWithValue("@email", mail);
+                InsertUsers.Parameters.AddWithValue("@pass",GetSha256FromString("siojjr"));
+                InsertUsers.ExecuteNonQuery();
+                MySqlCommand selectUsers = new MySqlCommand("select id from aauth_users where email=@email",connexion);
+                selectUsers.Parameters.AddWithValue("@email", mail);
+                MySqlDataReader rdr= selectUsers.ExecuteReader();
+                rdr.Read();
+                int idAuth = Convert.ToInt32(rdr["id"]);
+                rdr.Close();
+                MySqlCommand InsertEleves = new MySqlCommand("insert into eleve(nom,prenom,login,idClasse,idAuth) values (@nom,@prenom,@login,@idClasse,@idAuth)",connexion);
+                InsertEleves.Parameters.AddWithValue("@nom", nom);
+                InsertEleves.Parameters.AddWithValue("idClasse", test[0]);
+                InsertEleves.Parameters.AddWithValue("@login", mail);
+                InsertEleves.Parameters.AddWithValue("@prenom", prenom);
+                InsertEleves.Parameters.AddWithValue("@idAuth",idAuth);
+                InsertEleves.ExecuteNonQuery();
+
+                MySqlCommand InsertUserGroup = new MySqlCommand("insert into aauth_user_to_group(user_id,group_id) values(@user_id,@group_id)",connexion);
+                InsertUserGroup.Parameters.AddWithValue("@user_id", idAuth);
+                InsertUserGroup.Parameters.AddWithValue("@group_id",4);
+                InsertUserGroup.ExecuteNonQuery();
+            }
+            connexion.Close();
+        }
+
+        public static string GetSha256FromString(string data)
+        {
+            var message = Encoding.ASCII.GetBytes(data);
+            SHA256Managed hashString = new SHA256Managed();
+            string hex = "";
+            var hashValue = hashString.ComputeHash(message);
+            foreach (byte x in hashValue)
+            {
+                hex += String.Format("{0:x2}", x);
+            }
+            return hex;
+        }
+
+        public void renameAuteur()
+        {
+            string sConnexion;
+            MySqlConnection connexion;
+            sConnexion = "user=admin;password=siojjr;database=rallyelecture;host=172.16.0.159";
+            connexion = new MySqlConnection(sConnexion);
+            connexion.Open();
+            MySqlCommand selectAuteur = new MySqlCommand("select * from auteur", connexion);
+            MySqlDataReader rdr = selectAuteur.ExecuteReader();
+            while (rdr.Read())
+            {
+                string nom = Convert.ToString(rdr["nom"]);
+                string nouveauNom = "";
+                if (nom[0]=='d' && nom[1]=='e' && nom[2]==' ')
+                {  
+                    for (int i = 3; i < nom.Length; i++)
+                    {
+                        nouveauNom += nom[i];
+                    }
+                    MySqlCommand updateAuteur = new MySqlCommand("update auteur set nom=@nom where id=@id", connexion);
+                    updateAuteur.Parameters.AddWithValue("@nom", nouveauNom);
+                    updateAuteur.Parameters.AddWithValue("@id", Convert.ToInt32(rdr["id"]));
+                }
+            }
+            connexion.Close();
+        }
+    }
+}
